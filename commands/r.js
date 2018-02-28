b35555906cd95abd88152ec26f0367b2abf23e01
Glitch (new-bot) <none>
2018-02-28 12:39:12 +0000

    😹🚜 Checkpoint server.js:510018/8 watch.json:510018/2 commands/ping.js:510018/18 commands/r.js:510018/33 Storage/commands/delete.js:510018/14 commands/cinfo.js:510018/1

---
+++ commands/r.js
@@ -0,0 +1,14 @@
+module.exports.run = async (bot, message, args) => {
+  
+   if(message.author.id !== '322203879208910849') return message.reply("I'm sorry, but this command is only for the developer.")
+
+    if (!args.join(' ')) {
+      message.channel.send(`:x: Provide a command name to reload.`);
+      return;
+    }
+    delete require.cache[require.resolve(`./${args[0]}.js`)];
+    message.channel.send(`<a:update:413263871365611520> The command \`${args[0]}\` has been reloaded!`);
+}
+module.exports.config = {
+  command: "r"
+}
