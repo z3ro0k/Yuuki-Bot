a71b094fc8653d6d61600248b94e9cf437a093bd
Glitch (new-bot) <none>
2018-02-28 12:49:26 +0000

    🍁👓 Checkpoint Storage/commands/delete.js:510018/0 commands/cinfo.js:510018/93 commands/r.js:510018/7 watch.json:510018/2 server.js:510018/11 commands/rinfo.js:510018/91

--- commands/cinfo.js
+++ commands/cinfo.js
@@ -3,19 +3,25 @@ const Discord = require('discord.js');
 exports.run = (bot, message, args, func) => {
 let server = message.guild
 let canal = message.mentions.channels.first()
-if (!canal) message.channel.send('**Please mention a Channel**\n *>channelinfo #channel')
+if (!canal) {
+  message.channel.send('**Please mention a Channel**\n *>channelinfo #channel')
+  return; 
+}
 
   const embed= new Discord.RichEmbed() 
     .addField('Channel Name', canal.name)
-    .addField('Position', canal.position)
+    .addField('Position', canal.position = canal.position + 1)
     .addField('Topic', canal.topic)
     .addField('Type', canal.type)
     .addField('Category', canal.parent)
     .addField('Channel ID', canal.id)
     .setColor(0xFFFFFF)
-    .setThumbnail(server.iconURL())
-    .setAuthor(`Requested by ${message.author.tag}`, message.author.displayAvatarURL())
+    .setThumbnail(server.iconURL)
+    .setAuthor(`Requested by ${message.author.tag}`, message.author.displayAvatarURL)
   
   message.channel.send({ embed })
   
+}
+module.exports.config = {
+  command: "cinfo"
 }
