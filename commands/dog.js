240fd81e6da82f4402e2e9a56ed0c5edfde6a8ea
Glitch (yuukihost2) <none>
2018-05-16 04:05:48 +0000

    🚎🍁 Checkpoint ./commands/Dog.js:510018/160 ./package.json:510018/3730 ./watch.json:510018/2

--- commands/dog.js
+++ commands/dog.js
@@ -7,5 +7,10 @@ randomPuppy().then(url =>{
   const dog = new Discord.RichEmbed()
   .setImage(url)
   .setThumbnail(url)
-  .setDescription('Tu perro random esta aqui')
-      message.channel.send(url);
+  .setDescription('Tu perro random esta aqui<a:Perrosalu2:442155932495904768>')
+      message.channel.send(dog);
+})
+}
+exports.config = {
+  command: "dog"
+}
