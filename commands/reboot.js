8ce2666c24d64474815b0ce66fa8913dea770629
Glitch (yuukihost2) <none>
2018-05-16 01:06:26 +0000

    🔥🙄 Checkpoint ./commands/reboot.js:510018/311

--- commands/reboot.js
+++ commands/reboot.js
@@ -3,8 +3,10 @@ const fs = require('fs')
 module.exports.run = async (bot, message, args, loadCmds) => {
   
 const load = new Discord.RichEmbed()
-.setThumbnail('')
-.setTitle('Bot Status')
+.setThumbnail('https://media.discordapp.net/attachments/414868965479940096/446108369154211840/866945_game_512x512.png?width=65&height=65')
+.setTitle('Bot Status <a:001:402577432940511234>')
+.setDescription('All System Reload')
+message.channel.send(load)//.then
 loadCmds()
 
 }
