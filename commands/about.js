63b85f53b2563d66f5b200642b8928a6a8a739d3
Glitch (yuukihost2) <none>
2018-05-16 00:33:23 +0000

    🏜🚙 Checkpoint ./commands/about.js:510018/327 ./watch.json:510018/2

--- commands/about.js
+++ commands/about.js
@@ -11,17 +11,17 @@ const embed = new Discord.RichEmbed()
 .setColor(0x66ff66)
 
 .setAuthor(`Bot: ${bot.user.username} info`, bot.user.displayAvatarURL)
-.addField(`<:OwnerBot:372601959758430209>Owner`, '<@' + owner + '>', true)
-.addField(`<:GearRy:393126289214537738>Version`, `2.6.0`, true)
-.addField(`:books:Libreria`, `Discord ${Discord.version} (Js)`, true)
+.addField(`<:Owner:442443039915507743> Owner`, '<@' + owner + '>', true)
+.addField(`<:Version:442442898651217922> Version`, `2.6.0`, true)
+.addField(`:<:Libraries:442442996705918987> Libreria`, `Discord ${Discord.version} (Js)`, true)
 
 .addField(`<:plex:410957444441964545>Memoria`, `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
 .addField("CPU Usage", `${Math.round(cpu * 100) / 100}%`, true)
 .addField(`<:uptime:413201335597662210>Uptime`, `${actividad}`, true)
 
-.addField("<:commands:414861205073690624>Commands", bot.commands.size, true)
-.addField(`<:Box:413213626753744907>Guilds`, `${bot.guilds.size.toLocaleString()}`, true)
-.addField(`<:members:413201029488967682>Members`, `${bot.users.size.toLocaleString()}`, true)
+.addField("<:wEmoji:440388223017943042> Commands", bot.commands.size, true)
+.addField(`<:Servers:442443125005352962> Guilds`, `${bot.guilds.size.toLocaleString()}`, true)
+.addField(`<:members:442439950747697164> Members`, `${bot.users.size.toLocaleString()}`, true)
 .addField(`<:channels:413213800700051457>Channels`, `${bot.channels.size.toLocaleString()}`, true)
 
 message.channel.send({embed});
