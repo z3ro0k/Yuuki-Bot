d16f08c3b9805fec2dab22f2e9b2101d863e407e
Glitch (yuukihost2) <none>
2018-05-16 05:09:15 +0000

    ❣️🚥 Checkpoint ./commands/setstream.js:510018/372 ./watch.json:510018/6

--- commands/setstream.js
+++ commands/setstream.js
@@ -6,14 +6,7 @@ exports.run = async (bot, message, args) => {
 
       if(!args.join(' ')) return message.channel.send(`Escriba el estado.`);
     
-    bot.user.user.setActivity({
-          status: "dnd",
-          activity: {
-            name: args.join(' '),
-            type: 0,
-            url: "https://twitch.tv/ToXicGMDyt"
-          }
-        })
+   bot.user.setActivity(args.join(' '), { url: 'https://twitch.tv/ToXicGMDyt' })
     const embed = new Discord.RichEmbed()
     
     .addField("Cambiando mi estado a ", "<a:Streaming:446126986482417676>Streaming")
