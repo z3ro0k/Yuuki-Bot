6e7e50ac8f274b6e7e9a88a3da9fe94d6e2ae06c
Glitch (yuukihost2) <none>
2018-05-16 04:16:32 +0000

    🗾☺️ Checkpoint ./commands/invite.js:510018/961

---
+++ commands/invite.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js')
+
+exports.run = async (bot, message, args) => {
+var rpts = ["ff00ff", "00ffff", "8600b3", "754785", "e699ff", "ff3333", "006600", "8080ff"," 0000b3"];
+ var color = rpts[Math.floor(Math.random() * rpts.length)]
+ const embed = new Discord.RichEmbed()
+    .setAuthor('Invitacion de: ' + bot.user.username + '#' + bot.user.discriminator)
+    .setThumbnail(bot.user.avatarURL)
+    .setDescription('▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔')
+    .setDescription(`<:Discord_invite:352338233889128448>[Server Soporte](https://discord.gg/abyRgJ8)\n<:enlaces:352376308346519552>[Invitacion del Bot](https://discordapp.com/oauth2/authorize?client_id=365949788807757834&scope=bot&permissions=2084043903)`)
+    .setColor(color)
+ message.channel.send({embed})  
+}
+exports.config = {
+  command: "invite"
+}
