1d8af3f20bd14f59c152bd67206ec6402684cc1d
Glitch (yuukihost2) <none>
2018-05-16 03:01:52 +0000

    💳👩‍❤️‍💋‍👩 Checkpoint ./commands/radio.js:510018/67 ./package.json:510018/27 ./watch.json:510018/2 ./commands/kiss.js:510018/3218

--- commands/radio.js
+++ commands/radio.js
@@ -5,10 +5,10 @@ exports.run = async (bot, message, args) => {
       if(!voiceChannel) return message.channel.send('¡Necesitas unirte a un canal de voz primero!.');
       voiceChannel.join().then(conexion =>{
       conexion.playStream('http://stream.electroradio.fm:80/192k/;');
-      message.channel.send('<:musicNep:372597872849584148> ** | Radio electro activado.**')
+      message.channel.send('<:umEmoji:440388171587649536> ** | Radio electro activado.**')
             return;
           }).catch(console.error);
 }
 module.exports.config = {
-  command:
+  command: "radio"
 }
