8f722be71a83292d774596ccf5255151265c4ca6
Glitch (yuukiv3) <none>
2018-06-04 01:39:32 +0000

    🤒😱 Checkpoint ./commands/fortnite.js:510018/230 ./watch.json:510018/8

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -4,18 +4,25 @@ const fortnite = new Client(key.FORTNITE)
 
 exports.run = async (bot, message, args) => {
   await message.delete();
-  if(message.author.id != "") return;
+  if(message.author.id != "322203879208910849") return;
   let username = args[0];
   let platform = args[1] || "pc";
   
   if(!username) return message.reply('**please provide a username.**')
   
   let data = fortnite.user(username, platform).then(data => {
-    console.log(data)
+    
+    let stats = data.stats;
+    let lifetime = stats.lifetime;
+    console.log(lifetime)
+    let top3 = lifetime[0]['Top 3'];
+    console.log(top3)
+    let Score
+    
   });
   
 };
 exports.config = {
   command: "fortnite",
-  aliases: ["fortnite", "Fort"]
+  aliases: ["fortnite", "fort"]
 };
