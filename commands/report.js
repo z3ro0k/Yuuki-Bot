80bfaa411b56f7102e1e8071c5ea727ed6c364a3
Glitch (yuukiv3) <none>
2018-06-04 06:25:07 +0000

    🎢🍫 Checkpoint ./commands/upvote.js:510018/209 ./commands/report.js:510018/8409

---
+++ commands/report.js
@@ -0,0 +1,31 @@
+
+const Discord = require('discord.js')
+exports.run = async (bot, message, prefix, func) => { 
+await message.delete();
+  
+  let msg =  message.content.slice(prefix.length).trim().split("|");
+  let razon = msg[0];
+  let sug = msg[1];
+  let link = msg[2]; 
+
+  if (!razon) return message.channel.send('⚠ **command**, `Yu!report comando | razon | pruebas, si no cuentas con evidencias pon nn`');
+  if (!sug) return message.channel.send('⚠ **razon**, `Yu!report comando | razon | pruebas, si no cuentas con evidencias pon nn`');
+  if (!link) return message.channel.send('⚠ **Link**, `Yu!report comando | razon | pruebas, si no cuentas con evidencias pon nn`'); 
+    
+     const embed = new Discord.MessageEmbed()
+    .setAuthor('Bug enviado por: ' + message.author.username + '#' + message.author.discriminator)
+    .setThumbnail(message.author.displayAvatarURL())
+    .setDescription('▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔')
+    .addField('ID:',message.author.id)
+    .addField('Razon:',razon)
+    .addField('Por:',sug)
+    .addField('Pruebas','[Imagen]('+ link + ')')
+    .setTimestamp()
+    .setColor(0x36393e)
+    bot.channels.get('393551417887162368').send({embed});
+    message.channel.send('**:white_check_mark: Su informe se envía correctamente, espere mientras el desarrollador responde**')
+ } 
+exports.config = {
+  command: "report",
+  aliases: ["report", "bug", "bugreport", "reportbug"]
+}
--- commands/upvote.js
+++ commands/upvote.js
@@ -3,13 +3,13 @@ exports.run = async (client, message, args ) => {
   
 	 let emoji = client.emojis.find(e => e.name === 'Upvote');
    let embed = new Discord.MessageEmbed()
-   .setTitle('Veo que quieres votar por el Bot. Upvoting me permite:')
-   .addField('Para que sirve', '• Para que el bot cresca y tenga mas usuarios')
-   .addField('• El bot es más conocido', + client.guilds.size + ' Server actuales')
+   .setTitle('Veo que quieres votar por el Bot. Upvoting me permite')
+   .addField('Para que sirve', '• El bot sera más conocido.')
+   .addField('• Para que el bot cresca', 'Cuenta con ' + client.guilds.size + ' guilds actuales')
    .addField(`${emoji}`, '• ¡Obtienes más características increíbles todos los días!')
-   .addField('Upvote', '[here](https://discordbots.org/bot/365949788807757834) | [WebSite](https://yuukibot.github.io/) 
+   .addField('Upvote', '[here](https://discordbots.org/bot/365949788807757834) | [WebSite](https://yuukibot.github.io/) | [GITHUB]()')
    .setColor(0x36393e)
-   .setThumbnail(client.user.avatarURL)
+   .setThumbnail(client.user.avatarURL())
    message.channel.send({embed})
 	}
 exports.config = {
