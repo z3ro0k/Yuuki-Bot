d540f150bbe0d6fd376e08aa5c1b72c7a6207620
Glitch (yuukiv3) <none>
2018-06-04 01:28:53 +0000

    📌🎻 Checkpoint ./data/apis.json:510018/52 ./commands/fortnite.js:510018/606 ./package.json:510018/2373 ./watch.json:510018/4

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -1,3 +1,21 @@
-const key = require('./data/apis.json')
+const key = require('../data/apis.json')
 const Client = require('fortnite')
-const fortnite = new Client(key.fortnite)
+const fortnite = new Client(key.FORTNITE)
+
+exports.run = async (bot, message, args) => {
+  await message.delete();
+  if(message.author.id != "") return;
+  let username = args[0];
+  let platform = args[1] || "pc";
+  
+  if(!username) return message.reply('**please provide a username.**')
+  
+  let data = fortnite.user(username, platform).then(data => {
+    console.log(data)
+  });
+  
+};
+exports.config = {
+  command: "fortnite",
+  aliases: ["fortnite", "Fort"]
+};
