49d64223570a09c7d4a1defe94c3c1edbd30b78a
Glitch (yuukiv3) <none>
2018-06-06 04:31:22 +0000

    🚂🏨 Checkpoint ./commands/setavatar.js:510018/33 ./eventos/ready.js:510018/78 ./watch.json:510018/2

--- commands/setavatar.js
+++ commands/setavatar.js
@@ -4,17 +4,17 @@ exports.run = (bot, message, args, func) => {
 
     if(message.author.id !== '322203879208910849') return;
                                 
-   let link = (message.attachments.first().url).then(link => {
-   bot.user.setAvatar(link);
+   let link = message.attachments.first().url
+   bot.user.setAvatar(link.url);
    const embed = new Discord.MessageEmbed()
    .setTitle("『Set Avatar』")
    .setColor(0x36393e)
    .setTimestamp()
-   .setImage(link)
+   .setImage(link.url)
    .setFooter('New avatar set')
    message.channel.send({embed})
    message.delete();
-  })
+
 }
 exports.config = {
   command: "setavatar",
