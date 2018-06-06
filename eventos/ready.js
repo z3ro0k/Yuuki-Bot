b7237af6301d74f3adba9272cde1be6643ded1cc
Glitch (yuukiv3) <none>
2018-06-06 01:14:29 +0000

    🚐🚠 Checkpoint ./watch.json:510018/6 ./eventos/ready.js:510018/1384 ./server.js:510018/3649 ./eventos/messageReactionAdd.js:510018/4420 ./eventos/messageReactionRemove.js:510018/3364

---
+++ eventos/ready.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js')
+const prefix = '.'
+exports.run = (bot, guild, args, tools) => {
+
+console.log('Bot launched...')
+  
+bot.user.setPresence({
+          status: "dnd",
+          activity: {
+            name: prefix + `help | ${bot.guilds.size} guilds 📝 | ${bot.users.size} usuarios 👤 | Yu!invite`,
+            type: 2
+          }
+        }) 
+}
--- server.js
+++ server.js
@@ -81,17 +81,6 @@ bot.on('message', message => {
   
 
 })
-bot.on('ready', () =>{
-console.log('Bot launched...')
-  
-bot.user.setPresence({
-          status: "dnd",
-          activity: {
-            name: prefix + `help | ${bot.guilds.size} guilds 📝 | ${bot.users.size} usuarios 👤 | Yu!invite`,
-            type: 2
-          }
-        })  
-});
 
 bot.login('MzcwODI5Mzc5Mjc5OTEyOTcx.DdvsgQ.vEjUN5QR04xpa3JmC7dCCZehv4c')
 //bot.login('Mzk2NTA1Mjc3MjYxOTM4Njg5.DXgWmw.e1zXkHOcrXJEY3635Pu8-cFWAgQ') //Supreme Bot token
@@ -113,83 +102,7 @@ bot.on('channelDelete', async channel => {
   
   
 })
-// Starboard: Add
-bot.on('messageReactionAdd', async (messageReaction, user) => {
-  console.log(messageReaction)
-  if (messageReaction.emoji.toString() !== '⭐') return; // Incorrect Emoji
 
-  // Fetch Data
-  let item = await db.fetch(`starItem_${messageReaction.message.id}`)
-  let target = await db.fetch(`starboard_${messageReaction.message.guild.id}`)
-  let requiredRole = await db.fetch(`starStarter_${messageReaction.message.guild.id}`)
-  let channel = messageReaction.message.guild.channels.get(target.channel) || false;
-  
-  // Return Statements
-  if (!target.enabled) return; // Not Enabled
-  if (!channel) return; // No Channel
-  
-  const embed = new Discord.MessageEmbed()
-      .setColor(0x36393e)
-      .setTitle('Starboard')
-  
-  if (messageReaction.message.author.id === user.id) {
-    embed.setFooter('¡No puedes reacionar a tu propio mensaje!')
-    return messageReaction.message.channel.send(embed).then(msg => {
-      msg.delete({timeout: 10000})
-    })
-  }
-  
-  let hasRole;
-  if (requiredRole === null) hasRole = true;
-  else if (messageReaction.message.guild.members.get(user.id).roles.find(role => role.name === requiredRole)) hasRole = true;
-  else hasRole = false;
-  
-  if (item === null && hasRole) { // Starboard message not created
-    db.set(`starItem_${messageReaction.message.id}`, { reactions: 1, reactants: [user.tag], message: { id: messageReaction.message.id, content: messageReaction.message.content }, author: { id: messageReaction.message.author.id, tag: messageReaction.message.author.tag } })
-    
-      embed.setDescription(messageReaction.message.content)
-      .addField('Estrellas', '`⭐1`', true)
-      .setFooter(`Reactivos: ${user.tag}`)
-      .addField('Autor', messageReaction.message.guild.members.get(messageReaction.message.author.id), true)
-    
-    if (messageReaction.message.attachments.first()) {
-      db.set(`starItem_${messageReaction.message.id}`, messageReaction.message.attachments.first().url, { target: '.attachment' })
-      embed.setImage(messageReaction.message.attachments.first().url)
-    }
-    
-    channel.send(embed).then(msg => {
-      db.set(`starItem_${messageReaction.message.id}`, msg.id, { target: '.embedID' })
-    })
-  } else if (item !== null) {
-    console.log(item)
-    let msgID = messageReaction.message.id;
-    
-    // Configure Database
-    await db.add(`starItem_${msgID}`, 1, { target: '.reactions' })
-    let newItem = await db.fetch(`starItem_${msgID}`)
-    newItem.reactants.push(user.tag)
-
-      embed.setDescription(newItem.message.content)
-      .addField('Estrellas', `\`⭐${newItem.reactions}\``, true)
-      .setFooter(`Reactivos: ${newItem.reactants.join(', ')}`)
-      .addField('Autor', messageReaction.message.guild.members.get(newItem.author.id), true)
-      
-      if (newItem.attachment) embed.setImage(newItem.attachment)
-    
-    bot.channels.get(target.channel).messages.get(newItem.embedID).edit(embed)
-    
-    db.set(`starItem_${msgID}`, newItem)
-    
-  } else if (!messageReaction.message.guild.members.get(user.id).roles.find(role => role.name === 'Moderators')) {
-    embed.setFooter(`Lo sentimos, solo las personas con el rol ${requiredRole} pueden ser los primeros en destacar el mensaje.`)
-    return messageReaction.message.channel.send(embed).then(msg => {
-      msg.delete({timeout: 10000})
-    })
-  }
-  
-});
-
-// Starboard: Remove
 bot.on('messageReactionRemove', async (messageReaction, user) => {
   if (messageReaction.emoji.toString() !== '⭐') return; // Incorrect Emoji
 
