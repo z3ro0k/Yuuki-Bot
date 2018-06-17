dadc0990973acb18b29a2613a1945286015c4d94
Glitch (yuuki-host-new) <none>
2018-06-17 00:06:53 +0000

    🎙🌊 Checkpoint ./commands/help.js:510018/1116

--- commands/help.js
+++ commands/help.js
@@ -1,8 +1,8 @@
 
 const Discord = require("discord.js");
 const { stripIndents } = require('common-tags');
-    let pages = ['◈ `Yu!ping`◈ `Yu!avatar` ◈ `Yu!ascii`◈ `Yu!calc◈ `To!emojis` ◈ `Yu!playing` ◈ `Yu!jumbo` ◈ `Yu!hug` ◈ `Yu!fortnite` ◈ `Yu!cat` ◈ `Yu!dog`'];
-    let info = ['◈ `Yu!spotify`  ::  Muestra la canción que estás escuchando en spotify el usuario mencionado\n◈ `Yu!weather`  ::  Muestra información sobre una ciudad\n◈ `Yu!help`     ::  Muestra todos los comandos disponibles\n◈ `Yu!npm`      ::  busca un paquete para agregar a tu bot\n◈ `Yu!user` ::  muestre su información o la del usuario mencionado\n◈ `Yu!ytsearch` ::  buscar canciones de youtube con el bot\n◈ `Yu!google`   ::  Busca algo en google\n◈ `Yu!mcuser`   ::  ¡Obtén la información de un usuario de minecraft!\n◈ `Yu!roblox`   ::  busca un usuario roblox y obtén información']
+    let pages = ['◈ `Yu!ping` ◈ `Yu!avatar` ◈ `Yu!ascii`◈ `Yu!calc◈` `Yu!emojis` ◈ `Yu!playing` ◈ `Yu!jumbo` ◈ `Yu!hug` ◈ `Yu!fortnite` ◈ `Yu!cat` ◈ `Yu!dog`'];
+    let info = ['◈ `Yu!spotify` ◈ `Yu!weather` ◈ `Yu!help` ◈ `Yu!npm` ◈ `Yu!user` ◈ `Yu!ytsearch` ◈ `Yu!google` ◈ `Yu!mcuser` ◈ `Yu!roblox`']
     let admin = ['== Admin Commands ==\n\n◈ `Yu!hook`    :: envía un weekhook con tus argumentos\n◈ `Yu!settings`  ::  muestra la configuración actual del bot en su servidor\n◈ `Yu!tmute` ::  Silenciar al usuario mencionado por x tiempo\n◈ `Yu!warn`    ::  Advierte al usuario mencionado\n◈ `Yu!warnlist` ::  Muestra la lista de warns del usuario mencionado \n◈ `Yu!clean`    ::  borras mensajes con el bot']
     let nsfw = ['== NSFW Commands ==\n\n◈ No hay comandos NSFW por el momento']
     let botC = ['== BOT Commands ==\n\n◈ `Yu!report`  :: Usted envía un error que tiene el bot\n◈ `Yu!suggestion` :: Envía una sugerencia para agregar un comando al bot\n◈ `Yu!about`    :: Muestra toda la informacion del bot']
@@ -15,7 +15,7 @@ exports.run =  async (bot, message, args) => {
    await message.delete();
 if(!command) {
     const embed = new Discord.MessageEmbed()
-      .setDescription(`<:helpNEP3:372992359287488512>**Mis comandos son **<:helpNEP3:372992359287488512>`)
+      .setDescription(`<:helpNEP3:372992359287488512>**Mis comandos son **<:helpNEP3:372992359287488512>\nUse Yu!help comando para ver información detallada sobre el comando.`)
       .addField("== Comandos de Diversion ==", pages)
      // .addField("== Info Commands ==", info)
       .addField("Yu!help music","Muestra los comandos de musica")
@@ -27,24 +27,18 @@ if(!command) {
   			try {
 				//const msgs = [];
 				message.author.send({ embed });
-				if (message.channel.type !== 'dm') return await message.reply('�� Sent you a DM with information.');
+				if (message.channel.type !== 'dm') return await message.reply('�� Te envié un DM con la lista de mis comandos');
 			} catch (err) {
-				return message.reply('Failed to send DM. You probably have DMs disabled.');
+				return message.reply('Error al enviar DM Probablemente tengas DMs deshabilitados.');
 			}
 		}
-  const embed = new Discord.MessageEmbed()
-  .addFiled("Comando:", `**${command.config.command}**`)
+  const embed2 = new Discord.MessageEmbed()
+  .addField("Comando:", `**${command.config.command}**`)
   .addField("Alias:", `${command.config.aliases.join(', ') || 'None'}`)
   .addField("Categoria:", `(\`${command.config.category}\`)`)
-  .addField("Descripción:", `**${command.config.description}**`)
-  
-  
-  
- return message.channel.send(stripIndents`
-			__Command **${command.config.command}**__${command.guildOnly ? ' (Usable only in servers)' : ''}
-			${command.config.description} **Aliases**: ${command.config.aliases.join(', ') || 'None'}
-			**Group**: (\`${command.config.category}\`)
-		`);
+  .addField("Descripción:", `**${command.config.description}**`) 
+  .setThumbnail(bot.user.displayAvatarURL())
+ return message.channel.send( embed2 );
 	}
 exports.config = {
   command: "help",
