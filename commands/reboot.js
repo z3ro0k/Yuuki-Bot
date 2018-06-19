14928df51c963f13f2d4a85bc1cc5a679e64ccfe
Glitch (yuuki-host-new) <none>
2018-06-19 23:54:25 +0000

    🐧😼 Checkpoint ./watch.json:510018/4 ./commands/reboot.js:510018/201

--- commands/reboot.js
+++ commands/reboot.js
@@ -1,39 +1,43 @@
 const Discord = require('discord.js')
 const fs = require('fs')
 module.exports.run = async (bot, message, args, tools, loadCmds, eventsLoad) => {
-if(args[0] === 'commanads' || args[0] === 'comandos'){
-  message.channel.send('<a:loader:458776406512369664> Rebooting Commands').then(m => {
+if(args[0] === 'commands' || args[0] === 'comandos'){
+ return message.channel.send('<a:loader:458776406512369664> Rebooting Commands').then(m => {
+    
     setTimeout(() => {
+       m.delete();
   const loadC = new Discord.MessageEmbed()
 .setThumbnail('https://cdn.dribbble.com/users/143640/screenshots/2666261/fueled-24112015-jm_2x.gif')
 .setTitle('Revooting Commands')
 .addField('<:kEmoji:440388066197110785> Commands loaded', bot.commands.size + 11, true)
-  .addField('<:kEmoji:440388066197110785> Alias Loaded', bot.aliases.size + 11, true)
+  .addField('<:kEmoji:440388066197110785> Alias Loaded', bot.aliases.size , true)
 .setColor(0x36393e)
-m.edit(loadC)
+m.channel.send(loadC)
       loadCmds();
     }, 5000);
   });
   
 }
 if(args[0] === 'events' || args[0] === 'eventos'){
-  message.channel.send('<a:loader:458776406512369664> Rebooting Events').then(m => {
+ return message.channel.send('<a:loader:458776406512369664> Rebooting Events').then(m => {
+    
     setTimeout(() => {
+       m.delete();
   const loadE = new Discord.MessageEmbed()
 .setThumbnail('https://cdn.dribbble.com/users/143640/screenshots/2666261/fueled-24112015-jm_2x.gif')
 .setTitle('Rebooting Events')
 .addField('<:doc:448784570188562433> Charged events', bot.commands.size + 11, true)
 .setColor(0x36393e)
-m.edit(loadE)
+m.channel.send(loadE)
       eventsLoad();
     }, 5000);
   });
   
 } 
-  if(args[0] === 'all' || args[0] === 'all'){
- message.channel.send('<a:loader:458776406512369664> Rebooting Commands').then(m => {
+  if(args[0] === 'all' || args[0] === 'all') {
+return message.channel.send('<a:loader:458776406512369664> Rebooting all System').then(m => {
     setTimeout(() => {
-      
+      m.delete();
     const allload = new Discord.MessageEmbed()
 .setThumbnail('https://cdn.dribbble.com/users/143640/screenshots/2666261/fueled-24112015-jm_2x.gif')
 .setTitle('Rebooting All System')
@@ -41,7 +45,7 @@ m.edit(loadE)
 .addField('<:kEmoji:440388066197110785> Comandos Cargados', bot.commands.size + 11, true)
 .addField('<:doc:448784570188562433> Eventos cargados', bot.events.size, true)
 .setColor(0x36393e)
-m.edit(allload)
+m.channel.send(allload)
       loadCmds();
       eventsLoad();
       }, 5000);
