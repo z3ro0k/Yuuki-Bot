4a3008dd5714f7dce77c205b907ac153e5a63c9d
Glitch (yuuki-host-new) <none>
2018-06-19 23:44:19 +0000

    🍓🎻 Checkpoint ./commands/reboot.js:510018/794 ./watch.json:510018/4

--- commands/reboot.js
+++ commands/reboot.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js')
 const fs = require('fs')
 module.exports.run = async (bot, message, args, tools, loadCmds, eventsLoad) => {
-if(args[0].toLowerCase() === 'commanads' || args[0].toLowerCase() === 'comandos'){
-  message.channel.send('<a:loader:458776406512369664> Actualizando Comandos').then(m => {
+if(args[0] === 'commanads' || args[0] === 'comandos'){
+  message.channel.send('<a:loader:458776406512369664> Rebooting Commands').then(m => {
     setTimeout(() => {
   const loadC = new Discord.MessageEmbed()
 .setThumbnail('https://cdn.dribbble.com/users/143640/screenshots/2666261/fueled-24112015-jm_2x.gif')
@@ -10,35 +10,46 @@ if(args[0].toLowerCase() === 'commanads' || args[0].toLowerCase() === 'comandos'
 .addField('<:kEmoji:440388066197110785> Commands loaded', bot.commands.size + 11, true)
   .addField('<:kEmoji:440388066197110785> Alias Loaded', bot.aliases.size + 11, true)
 .setColor(0x36393e)
-m.channel.send(load)
+m.edit(loadC)
       loadCmds();
     }, 5000);
   });
   
 }
-if(args[0].toLowerCase() === 'events' || args[0].toLowerCase() === 'eventos'){
-  message.channel.send('Actualizando Comandos').then(m => {
+if(args[0] === 'events' || args[0] === 'eventos'){
+  message.channel.send('<a:loader:458776406512369664> Rebooting Events').then(m => {
     setTimeout(() => {
-  const loadC = new Discord.MessageEmbed()
+  const loadE = new Discord.MessageEmbed()
 .setThumbnail('https://cdn.dribbble.com/users/143640/screenshots/2666261/fueled-24112015-jm_2x.gif')
 .setTitle('Rebooting Events')
 .addField('<:doc:448784570188562433> Charged events', bot.commands.size + 11, true)
 .setColor(0x36393e)
-m.channel.send(load)
+m.edit(loadE)
+      eventsLoad();
     }, 5000);
   });
   
 } 
-const load = new Discord.MessageEmbed()
+  if(args[0] === 'all' || args[0] === 'all'){
+ message.channel.send('<a:loader:458776406512369664> Rebooting Commands').then(m => {
+    setTimeout(() => {
+      
+    const allload = new Discord.MessageEmbed()
 .setThumbnail('https://cdn.dribbble.com/users/143640/screenshots/2666261/fueled-24112015-jm_2x.gif')
-.setTitle('Bot Status')
+.setTitle('Rebooting All System')
 .setDescription('<a:Online:446119385480953866> All System Reload <:Ainfo:441067085163134976>')
 .addField('<:kEmoji:440388066197110785> Comandos Cargados', bot.commands.size + 11, true)
 .addField('<:doc:448784570188562433> Eventos cargados', bot.events.size, true)
 .setColor(0x36393e)
-message.channel.send(load)
-eventsLoad();
-
+m.edit(allload)
+      loadCmds();
+      eventsLoad();
+      }, 5000);
+    });
+  } else {
+   message.channel.send('Seleccione una categoría para reiniciar `Yu!reboot commands | events | all`') 
+    return;
+  }
 }
 module.exports.config = {
   command: "reboot",
