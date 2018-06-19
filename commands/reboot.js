dbbebfb759d4469aad20af9e8864874b94ff056e
Glitch (yuuki-host-new) <none>
2018-06-19 23:34:13 +0000

    🍦🛥 Checkpoint ./commands/reboot.js:510018/738

--- commands/reboot.js
+++ commands/reboot.js
@@ -2,10 +2,10 @@ const Discord = require('discord.js')
 const fs = require('fs')
 module.exports.run = async (bot, message, args, tools, loadCmds, eventsLoad) => {
 if(args[0].toLowerCase() === 'commanads' || args[0].toLowerCase() === 'comandos'){
-  message.channel.send('Actualizando Comandos').then(m => {
+  message.channel.send('<a:loader:458776406512369664> Actualizando Comandos').then(m => {
     setTimeout(() => {
   const loadC = new Discord.MessageEmbed()
-.setThumbnail('https://static1.squarespace.com/static/59eee47aa8b2b0c9f26337a7/t/59effa9051a584527a9a8096/1508899504685/Design+Icon+Ltd.+SIMPLE+PROCESS+Design+Hong+Kong')
+.setThumbnail('https://cdn.dribbble.com/users/143640/screenshots/2666261/fueled-24112015-jm_2x.gif')
 .setTitle('Revooting Commands')
 .addField('<:kEmoji:440388066197110785> Commands loaded', bot.commands.size + 11, true)
   .addField('<:kEmoji:440388066197110785> Alias Loaded', bot.aliases.size + 11, true)
@@ -20,7 +20,7 @@ if(args[0].toLowerCase() === 'events' || args[0].toLowerCase() === 'eventos'){
   message.channel.send('Actualizando Comandos').then(m => {
     setTimeout(() => {
   const loadC = new Discord.MessageEmbed()
-.setThumbnail('https://static1.squarespace.com/static/59eee47aa8b2b0c9f26337a7/t/59effa9051a584527a9a8096/1508899504685/Design+Icon+Ltd.+SIMPLE+PROCESS+Design+Hong+Kong')
+.setThumbnail('https://cdn.dribbble.com/users/143640/screenshots/2666261/fueled-24112015-jm_2x.gif')
 .setTitle('Rebooting Events')
 .addField('<:doc:448784570188562433> Charged events', bot.commands.size + 11, true)
 .setColor(0x36393e)
@@ -30,7 +30,7 @@ m.channel.send(load)
   
 } 
 const load = new Discord.MessageEmbed()
-.setThumbnail('https://static1.squarespace.com/static/59eee47aa8b2b0c9f26337a7/t/59effa9051a584527a9a8096/1508899504685/Design+Icon+Ltd.+SIMPLE+PROCESS+Design+Hong+Kong')
+.setThumbnail('https://cdn.dribbble.com/users/143640/screenshots/2666261/fueled-24112015-jm_2x.gif')
 .setTitle('Bot Status')
 .setDescription('<a:Online:446119385480953866> All System Reload <:Ainfo:441067085163134976>')
 .addField('<:kEmoji:440388066197110785> Comandos Cargados', bot.commands.size + 11, true)
