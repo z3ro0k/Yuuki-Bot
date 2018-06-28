ef55347a34cec56e7dfd84115481d67dbf86d4fe
Glitch (yuuki-host-new) <none>
2018-06-28 01:15:50 +0000

    ☂️☃️ Checkpoint ./commands/fetchuser.js:510018/1193

--- commands/fetchuser.js
+++ commands/fetchuser.js
@@ -1,14 +1,30 @@
 const Discord = require('discord.js')
-
+const { MessageEmbed,  escapeMarkdown } = require('discord.js');
 exports.run = async(bot, message, args, level) => {
   
   bot.users.fetch(args[0]).then(user => {
-    
+    var userStatus
+        if (user.presence.activity !== null) {
+      if (user.presence.activity.type === 'PLAYING') {
+        userStatus = `Playing **${escapeMarkdown(user.presence.activity.name)}**`
+      } else if (user.presence.activity.type === 'STREAMING') {
+        userStatus = `Streaming ${(user.presence.activity.name)}`
+      } else if (user.presence.activity.type === 'LISTENING') {
+        userStatus = `Listening to **${escapeMarkdown(user.presence.activity.name)}**`
+      } else if (user.presence.activity.type === 'WATCHING') {
+        userStatus = `Watching **${escapeMarkdown(user.presence.activity.name)}**`
+      }
+      if (user.presence.activity.url !== null) { userStatus = `[\`${userStatus}\`](${user.presence.activity.url})` }
+    }
+    if (!userStatus) {
+     userStatus = "User is not playing a game"
+ }
 const embed = new Discord.MessageEmbed()
 .addField('❯ User name', user.tag, true)
 .addField('❯ User ID', user.id, true)
-//.addField('❯ Discord Join Date', user.user.createdAt.toDateString(), true)
+.addField('❯ Discord Join Date', user.createdAt.toDateString(), true)
 .addField('❯ User status', user.presence.status, true)
+.addField('❯ User game', userStatus, true)
 .setThumbnail(user.displayAvatarURL())
 message.channel.send(embed)
   });
