adbe3e9b84b9cc924e5f1919e983b21b03a4c386
Glitch (yuuki-host-new) <none>
2018-07-01 23:44:30 +0000

    🐲😘 Checkpoint ./commands/fortnite.js:510018/5

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -4,7 +4,7 @@ const Client = require('fortnite')
 const fortnite = new Client(key.FORTNITE)
 
 exports.run = async (bot, message, args) => {
-  await message.delete();
+   message.delete();
   if(message.author.id != "322203879208910849") return;
   let username = args[0];
   let platform = args[1] || "pc";
