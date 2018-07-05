4ff70e92559a225e3d922af63cb1a16c3bf836df
Glitch (yuuki-host-new) <none>
2018-07-05 21:01:57 +0000

    👝🐲 Checkpoint ./commands/upvote.js:510018/664 ./data/apis.json:510018/60

--- commands/upvote.js
+++ commands/upvote.js
@@ -15,11 +15,15 @@ exports.run = async (client, message, args ) => {
             };  
   request(options, function(error, response, body) {
     
+    var certified = body.certifiedBot
+    if(certified === false) certified = '<:Not:463805182648778783> No'
+    if(certified === true) certified = '<:Certified:464296361899327498> Yes'
+    
     let embed = new Discord.MessageEmbed()
    .setTitle('Vote System')
    .setDescription(`**Vote system Características**\n• El bot sera más conocido.\n• Para que el bot cresca Cuenta con ${client.guilds.size} guilds actuales\n• ¡Obtienes más características increíbles todos los días!`)
-   .addField('Discord Bots List',  `${emoji} Upvotes ${body.points}`)
-   .addField('Upvote', '[`here`](https://discordbots.org/bot/365949788807757834) | [`WebSite`](https://yuukibot.github.io/) | [`GITHUB`](https://yuukibot.github.io/)')
+   .addField('Discord Bots List',  `Upvotes: ${emoji}${body.points}\nCertified Bot: ${certified}\nLinks: ${body.invite.length !== 0 ? `[\`Invite\`](${body.invite}) | ` : ""}${body.website.length !== 0 ? `[\`Website\`](${body.website}) | ` : "" }${body.support.length !== 0 ? `[\`Support Server\`](https://discord.gg/${body.support})` : ""}`)
+
    .setColor(0x36393e)
    .setThumbnail(client.user.avatarURL())
    message.channel.send({embed})
