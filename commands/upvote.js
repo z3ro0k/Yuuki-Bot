70fb200078adc5894e20700711396ace1634f967
Glitch (yuuki-host-new) <none>
2018-07-05 20:51:22 +0000

    💘🌠 Checkpoint ./commands/upvote.js:510018/713 ./data/apis.json:510018/177

--- commands/upvote.js
+++ commands/upvote.js
@@ -1,20 +1,30 @@
 const Discord = require('discord.js')
+const apis = require('../data/apis.json')
 exports.run = async (client, message, args ) => {
   
 	 let emoji = client.emojis.find(e => e.name === 'Upvote');
   
-  
+  var request = require("request");
+            var options = {
+                method: 'GET',
+                url: `https://discordbots.org/api/bots/${client.user.id}/`,
+                json: true,
+                headers: {
+                    auth: `${apis.DBLAPI}`
+                }
+            };  
+  request(options, function(error, response, body) {
+    
     let embed = new Discord.MessageEmbed()
    .setTitle('Vote System')
-   .setDara que sirve', '• El bot sera más conocido.')
-   .addField('• Para que el bot cresca', 'Cuenta con ' + client.guilds.size + ' guilds actuales')
-   .addField(`${emoji}`, '• ¡Obtienes más características increíbles todos los días!')
-   .addField('Discord BOts List', a)
+   .setDescription(`**Vote system Características**\n• El bot sera más conocido.\n• Para que el bot cresca Cuenta con ${client.guilds.size} guilds actuales\n• ¡Obtienes más características increíbles todos los días!`)
+   .addField('Discord Bots List',  `${emoji} Upvotes ${body.points}`)
    .addField('Upvote', '[`here`](https://discordbots.org/bot/365949788807757834) | [`WebSite`](https://yuukibot.github.io/) | [`GITHUB`](https://yuukibot.github.io/)')
    .setColor(0x36393e)
    .setThumbnail(client.user.avatarURL())
    message.channel.send({embed})
-	}
+	})
+}
 exports.config = {
   command: "upvote",
   aliases: ['upvote', 'vote']
