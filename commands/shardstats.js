30421082bdb71cf164ccdf4463e6707c96ebb291
Glitch (yuuki-host-new) <none>
2018-07-06 19:46:01 +0000

    🏈💡 Checkpoint ./server_sharding.js:510018/422 ./data/apis.json:510018/81 ./package.json:510018/11 ./watch.json:510018/2 ./commands/info.js:510018/120 ./commands/shardstats.js:510018/2400

---
+++ commands/shardstats.js
@@ -0,0 +1,44 @@
+const columnify = require('columnify')
+const moment = require('moment')
+require('moment-duration-format')
+const Discord = require('discord.js')
+
+exports.run = async (bot, message, args) => {
+
+    // Check if bot is sharded or not.
+    if (!bot.shard) {
+      return message.reply('the bot is currently not sharded.')
+    }
+    // Get values from all shard.
+    var shardInfo = await bot.shard.broadcastEval(`[
+      this.shard.id,
+      this.guilds.size,
+      this.channels.size,
+      this.users.size,
+      (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2),
+      this.uptime
+    ]`)
+    // Create array of information of each shard.
+    var shardInfoArray = []
+    shardInfo.forEach(i => {
+      shardInfoArray.push({
+        id: i[0],
+        guilds: i[1],
+        channels: i[2],
+        users: i[3],
+        mem: i[4],
+        uptime: moment.duration(i[5]).format('y [yr,] M [mo,] w [wk,] d [day,] h [hr,] m [min,] s [sec, and] S [ms]')
+      })
+    })
+    // Send message with shard information using columns.
+    message.channel.send({
+      content: columnify(shardInfoArray, {
+        columnSplitter: ' │ '
+      }),
+      code: ''
+    })
+  }
+module.exports.config = {
+  command: "shardstats",
+  aliases: ['shardstats']
+}
---
+++ server_sharding.js
@@ -0,0 +1,12 @@
+const {ShardingManager} = require('discord.js');
+const config = require('./data/apis.json');
+
+const manager = new ShardingManager(`${__dirname}/server.js`, {token: config.TOKEN});
+
+manager.on('shardCreate', shard => console.log(`Successfully launched shard ${shard.id}`));
+
+manager.spawn().then(() => {
+    console.log(`Launched ${manager.totalShards} shards...`);
+}).catch((err) => {
+    console.log(err);
+});
