88424865e1d39a0ead0e36aa88778a44af12872f
Glitch (yuuki-host-new) <none>
2018-07-22 02:18:35 +0000

    ❣️💵 Checkpoint ./langs/es.json:510018/581 ./langs/en.json:510018/585 ./commands/about.js:510018/260

--- commands/about.js
+++ commands/about.js
@@ -2,15 +2,24 @@
 const moment = require("moment")
 const momen = require('moment-duration-format');
 const Discord = require('discord.js')
+const db = require('quick.db')
 
 exports.run = async (bot, message, args) => {
 
 const actividad = moment.duration(bot.uptime).format(" D [dias], H [hrs], m [mins], s [secs]");
-const cpu = process.cpuUsage().system / 1024 / 1024;    
+  
+const cpu = process.cpuUsage().system / 1024 / 1024;   
+  
    let emoji = bot.emojis.find(e => e.name === 'Upvote');
  var dev
     try { dev = bot.users.get('322203879208910849').tag } catch (e) { dev = 'PoeticAzurex#9169' }
   
+  var langg
+ const idioma = await db.fetch(`guildLang_${message.guild.id}`)
+ if (idioma === null) langg = 'es'
+  else langg = idioma       
+ const lang = require(`../langs/${langg}.json`) 
+ 
 const embed = new Discord.MessageEmbed()
 .setColor(0x36393e)
 .setAuthor(`Información de ${bot.user.username} `, bot.user.displayAvatarURL())
@@ -18,15 +27,15 @@ const embed = new Discord.MessageEmbed()
 .addField(`<:Version:442442898651217922> Version`, `2.6.0`, true)
 .addField(`<:Libraries:442442996705918987> Libreria`, `Discord ${Discord.version} (Js)`, true)
 .addField(`<:cloud:447518353972658207> Memoria`, `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
-.addField("<:CPU:462586915783180298> CPU Usage", `${Math.round(cpu * 100) / 100}%`, true)
+.addField(`<:CPU:462586915783180298> CPU Usage`, `${Math.round(cpu * 100) / 100}%`, true)
 .addField(`<:relog:447518519752523776> Tiempo de actividad`, `${actividad}`, true)
-.addField("<:wEmoji:440388223017943042> Comandos ", bot.commands.size + 11, true)
-.addField("<:doc:448784570188562433> Eventos", bot.events.size, true)
+.addField(`<:wEmoji:440388223017943042> Comandos`, bot.commands.size , true)
+.addField(`<:doc:448784570188562433> Eventos`, bot.events.size, true)
 .addField(`<:Servers:442443125005352962> Servidores`, `${bot.guilds.size.toLocaleString()}`, true)
 .addField(`<:members:442439950747697164> Miembros`, `${bot.users.size.toLocaleString()}`, true)
 .addField(`<:doc:448784570188562433> Canales`, `${bot.channels.size.toLocaleString()}`, true)
-.addField('<:partnerbot:447295492200595457> WebSite', '[Click aquí](https://brayanmaldonado9.wixsite.com/yuuki)', true)
-.addField('<:Astart:441067034554662932> Prefix', 'Yu!', true)
+.addField(`<:partnerbot:447295492200595457> WebSite`, `[Click aquí](https://brayanmaldonado9.wixsite.com/yuuki)`, true)
+.addField(`<:Astart:441067034554662932> Prefix', 'Yu!`, true)
 .addField(`${emoji} Vota por el bot`, 'Usa Yu!vote', true)
 
 message.channel.send({embed});
