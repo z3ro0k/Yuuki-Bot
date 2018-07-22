2eefc6ac61db1eb11700d97e67a5319e81643a5d
Glitch (yuuki-host-new) <none>
2018-07-22 09:31:42 +0000

    ⛵️🌖 Checkpoint ./commands/ban.js:510018/851 ./watch.json:510018/6 ./langs/es.json:510018/89

--- commands/ban.js
+++ commands/ban.js
@@ -1,28 +1,35 @@
 const Discord = require('discord.js')
 const db = require('quick.db')
-let bCase = 12;
 exports.run = async (bot, message, args) => {
 
-     var langg
+ var langg
  const idioma = await db.fetch(`guildLang_${message.guild.id}`)
  if (idioma === null) langg = 'es'
-  else langg = idioma       
+ else langg = idioma       
  const lang = require(`../langs/${langg}.json`)
+
+ const casenumbers = new db.table('CASENUMBERs')
+ const guildcasenumber = await casenumbers.fetch(`case_${message.guild.id}`)
+ const a = guildcasenumber
+ const b = a + 1
+ casenumbers.set(`case_${message.guild.id}`, b)
  
 let user = message.mentions.users.first();
 let razon = args[1]
 let perms = message.member.hasPermission("MANAGE_ROLES_OR_PERMISSIONS");
 
-if(!perms) return message.channel.send("<:adminNep:372599923381633024> **| No tienes Permisos para usar este comando.**");
+if(!perms) return message.channel.send(lang.noP.ban);
      
-if (message.mentions.users.size < 1) return message.reply("Debe mencionar a alguien <:bEmoji:440388028939239434>").catch(console.error);
-if(!razon) return message.channel.send('Escriba un razón, `Yu!ban @username [razón]`');
-if (!message.guild.member(user).bannable) return message.reply('No puedo banear al usuario mencionado.');
-                                      
+if (message.mentions.users.size < 1) return message.reply(lang.ban.men).catch(console.error);
+if(!razon) return message.channel.send(lang.ban.reazon);
+if (!message.guild.member(user).bannable) return message.reply(lang.ban.noBP);
+         
+  const modlog = message.guild.channels.find(channel => channel.name === 'mod-logs');
+  if (!modlog) return message.channel.send('**Please create a channel with the name `mod-logs`**')
                                   
  message.guild.member(user).ban(razon);
   let banEmbed = new Discord.MessageEmbed()
-    .setDescription(`**<:bEmoji:440388028939239434>Ban | Case #${bCase = bCase + 1}**`)
+    .setDescription(`**<:bEmoji:440388028939239434>Ban | Case #${guildcasenumber}**`)
     .setColor(0x36393e)
     .addField("Banned User", `${user} with ID ${user.id}`)
     .addField("Banned By", `<@${message.author.id}> with ID ${message.author.id}`)
@@ -30,7 +37,7 @@ if (!message.guild.member(user).bannable) return message.reply('No puedo banear 
     .addField("Reason", razon)
     .addField("Time", message.createdAt)
     
- message.channel.send(banEmbed);
+ modlog.send(banEmbed);
 }
 exports.config = {
   command: "ban",
