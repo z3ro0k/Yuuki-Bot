9ba6959f4f6e68ab1aae1434c673619bc93f3f07
Glitch (yuuki-host-new) <none>
2018-07-22 01:57:35 +0000

    🎸🐑 Checkpoint ./langs/es.json:510018/119 ./langs/en.json:510018/354 ./commands/8ball.js:510018/589 ./watch.json:510018/6

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,16 +1,24 @@
-const Discord = require('discord.js')
-
+const { MessageEmbed } = require('discord.js')
+const db = require('quick.db')
 exports.run = async (bot, message, args) => {
 
 const pregunta = args.join(' ')                              
 var rpts = ["Sí", "No", "¿Por qué?", "Por favor", "Tal vez", "No sé", "Definitivamente?", " ¡Claro! "," Sí "," No "," Por supuesto! "," Por supuesto que no "];
-
+var langg
+ const idioma = await db.fetch(`guildLang_${message.guild.id}`)
+ if (idioma === null) langg = 'es'
+  else langg = idioma       
+ const lang = require(`../langs/${langg}.json`) 
+ 
   if (!pregunta) {
-  message.reply(`Escriba una pregunta.`); 
+  const embed = new MessageEmbed()
+   .setDescription(`${lang.BALL.usage[0]} **${message.author.username}** ${lang.BALL.usage[1]}`)
+   .setColor(0x36393e)
+  message.channel.send( embed ); 
   return;
 }
-  const BallEmbed = new Discord.MessageEmbed()
-    .setColor(0x00FFFF)
+  const BallEmbed = new MessageEmbed()
+    .setColor(0x36393e)
     .setThumbnail('https://cdn.discordapp.com/emojis/396446449832951809.gif')
     .setAuthor('8ball', 'https://findicons.com/files/icons/1700/2d/512/8_ball.png')
     .addField('Pregunta:', pregunta)
