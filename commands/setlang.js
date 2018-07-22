3d312b3387e76a4de9c9eae406322bb63947f146
Glitch (yuuki-host-new) <none>
2018-07-22 01:26:18 +0000

    🌌🏰 Checkpoint ./commands/setlang.js:510018/846 ./watch.json:510018/24 ./langs/en.json:510018/9

--- commands/setlang.js
+++ commands/setlang.js
@@ -8,19 +8,38 @@ exports.run = async (client, message, args, tools) => {
  const idioma = await db.fetch(`guildLang_${message.guild.id}`)
  if (idioma === null) langg = 'es'
   else langg = idioma       
- const lang = require(`../../Lang/${langg}.json`)
+ const lang = require(`../langs/${langg}.json`) 
  
  let setLang = args[0] 
- if (!['en', 'es'].includes(args[0])) return message.channel.send({
+ if(setLang) {
+ return message.channel.send({
             embed: {
                 title: "ERROR!",
                 color: 0xE50000,
                 description: lang.langR.langR1
             }
         })
+ }
+ if (!['en', 'es'].includes(args[0])) return message.channel.send({
+            embed: {
+                title: "ERROR!",
+                color: 0xE50000,
+                description: lang.langR.langR2
+            }
+        })
   
+  const setL = setLang
+   db.set(`guildLang_${message.guild.id}`, setL)
+ var langg
+ const idioma2 = await db.fetch(`guildLang_${message.guild.id}`)
+ if (idioma2 === null) langg = 'es'
+else langg = idioma2
+  
+ const newLang = require(`../langs/${langg}.json`)   
+ 
   const embed = new MessageEmbed()
-   .setDescription(`mi lenguaje en este servidor es: **${langg}**` )
+    .setTitle(newLang.titleComp + '\n'+ newLang.lang.langUpdate)
+   .setDescription(newLang.lang.translate)
 message.channel.send(embed)
 }
 exports.config = {
