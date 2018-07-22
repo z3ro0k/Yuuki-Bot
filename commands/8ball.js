41441809cd6e3850946ba754a8dc0762a46cc132
Glitch (yuuki-host-new) <none>
2018-07-22 02:08:01 +0000

    😺🕋 Checkpoint ./langs/en.json:510018/472 ./watch.json:510018/10 ./langs/es.json:510018/282 ./commands/8ball.js:510018/557

--- commands/8ball.js
+++ commands/8ball.js
@@ -3,7 +3,6 @@ const db = require('quick.db')
 exports.run = async (bot, message, args) => {
 
 const pregunta = args.join(' ')                              
-var rpts = ["Sí", "No", "¿Por qué?", "Por favor", "Tal vez", "No sé", "Definitivamente?", " ¡Claro! "," Sí "," No "," Por supuesto! "," Por supuesto que no "];
 var langg
  const idioma = await db.fetch(`guildLang_${message.guild.id}`)
  if (idioma === null) langg = 'es'
@@ -17,12 +16,14 @@ var langg
   message.channel.send( embed ); 
   return;
 }
+  var rpts = lang.BALL.ress
+
   const BallEmbed = new MessageEmbed()
     .setColor(0x36393e)
     .setThumbnail('https://cdn.discordapp.com/emojis/396446449832951809.gif')
     .setAuthor('8ball', 'https://findicons.com/files/icons/1700/2d/512/8_ball.png')
-    .addField('Pregunta:', pregunta)
-    .addField('Respuesta:', rpts[Math.floor(Math.random() * rpts.length)])
+    .addField(lang.BALL.preg, pregunta)
+    .addField(lang.BALL.res, rpts[Math.floor(Math.random() * rpts.length)])
   message.channel.send( BallEmbed );          
 }
 exports.config = {
