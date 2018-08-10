e141cf143a73cbbc3cc6d5cf5b3afb897dedf508
Glitch (yuuki-host-new) <none>
2018-08-10 00:35:55 +0000

    🍋🏐 Checkpoint ./langs/en.json:510018/1727 ./watch.json:510018/4 ./langs/es.json:510018/231 ./commands/sprefix.js:510018/2533

---
+++ commands/sprefix.js
@@ -0,0 +1,34 @@
+const { MessageEmbed } = require('discord.js'),
+  db = require('quick.db')
+
+exports.run = async (client, message, args) => {
+
+ var langg = await client.tools.Lang(message.guild)    
+ const lang = require(`../langs/${langg}.json`) 
+ 
+ let setLang = args[0] 
+ if(!setLang) {
+ 
+   client.tools.getLang(message.channel, message.guild)
+   
+ } else {
+  
+ if (!['en', 'es'].includes(args[0])) return message.channel.send({
+            embed: {
+                title: "ERROR!",
+                color: 0xE50000,
+                description: lang.langR.langR2
+            }
+        })
+  
+  const setL = setLang
+  client.tools.langU(message.channel, message.guild, setL)
+ }
+}
+exports.config = {
+  command: "setlang",
+  aliases: ["setidioma", "lang"],
+  category: "beta",
+  description: " ",
+  usage: " "
+};
