bc8f9aea3bd1c676beab3107798721c0735333d6
Glitch (yuuki-host-new) <none>
2018-08-11 08:12:28 +0000

    🏩👙 Checkpoint ./commands/settings.js:510018/1715 ./watch.json:510018/4

--- commands/settings.js
+++ commands/settings.js
@@ -6,8 +6,14 @@ exports.run = async (bot, message, args) => {
   let mod
   let prefix = await bot.tools.GuildPrefix(message.guild)
   let channel
+  let Logs
   let starboardChannel
   
+  let ChannelID = await bot.tools.getWelcomeChannel(message.guild)
+
+  if (!message.guild.channels.get(ChannelID)) Logs = '**<:off:442082928323985408>  Not set**'
+  else Logs = message.guild.channels.get(ChannelID)  
+      
   let autoRoleU = await bot.tools.autoRoleUsers(message.guild)
   var User = '<:onn:442082974037573641> ' + autoRoleU
   
@@ -21,11 +27,7 @@ exports.run = async (bot, message, args) => {
 
         if (!welcomeSettingsFetched) mod  = '<:off:442082928323985408> Mod-logs disable'
         else mod = welcomeSettingsFetched
-    
-    db.fetch(`messageChannel_${message.guild.id}`).then(channelIDFetched => {
-
-        if (!message.guild.channels.get(channelIDFetched)) channel = '**<:off:442082928323985408>  Not set**'
-        else channel = message.guild.channels.get(channelIDFetched)     
+   
 
         db.fetch(`starboardChannel_${message.guild.id}`).then(stardboardIDFetchd => {
               if(!stardboardIDFetchd) starboardChannel = '**<:off:442082928323985408>  Not set**'
@@ -41,20 +43,23 @@ const settings = new Discord.MessageEmbed()
 .addField('Command:', prefix +'lang <idioma>', true)
 .addField('Command', prefix +'sprefix <newprefix>', true)
 .addField('Welcome Settings', mod, true)
-.addField('Channel', channel, true)
+.addField('Anti Invite', "Coming Soon..", true)
 .addField('Command:', prefix+'welcome', true)
-.addField('Command:', prefix + 'setlogs #channel',true)
+.addField('Command:', prefix+'antiinvite <false/true>', true)
+.addField('Welcome Logs', channel, true)
+.addField('Mod-Logs', Logs , true)
+.addField('Command:', prefix + 'welcomelogs #channel',true)
+.addField('Command:', prefix + 'modlogs #channel',true)
 .addField('Autorole Users', User, true)
 .addField('Autorole Bots', Bots, true)
+.addField('Command:', prefix + 'roleusers <rolename>', true)
+.addField('Command:', prefix + 'rolebots <rolename>', true)
 .addField('StarBoard', starboardChannel, true)
-.addField('Command:', prefix + 'sautorole <rolename>', true)
 .addField('Command:', prefix + 'starboard set #channel', true)
 .setColor(0x36393e)
 message.channel.send(settings)
           })          
         })
-      })
-  
 }
 module.exports.config = {
   command: "settings", 
