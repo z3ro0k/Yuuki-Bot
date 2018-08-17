af4226adc8d6e661a0749ce583f3ca81e963243b
Glitch (yuuki-host-new) <none>
2018-08-17 19:43:08 +0000

    🍻🍯 Checkpoint ./ccommands/reload.js:510018/271 ./watch.json:510018/6

--- ccommands/reload.js
+++ ccommands/reload.js
@@ -1,27 +1,31 @@
-module.exports.run = async (bot, msg, args) => {
+
+exports.run = async (bot, msg, args) => {
 
     if (msg.author.id !== bot.options.owner) return msg.channel.send('Sorry, only the owner can use this command.');
-
+var cmds = args.join(' ')
     try {
-        if (!args. || args.size < 1) return msg.channel.send("Must provide a command name to reload.");
+        if (!cmds || cmds < 1) return msg.channel.send("Must provide a command name to reload.");
+      
         let command = args[0]
+        
         delete require.cache[require.resolve(`./${command}`)]; // delete
         const cmd = require(`./${command}`); // require
+      
         bot.commands.delete(command); // delete from cache
         cmd.config.aliases.forEach(alias => {
             // delete aliases
             bot.aliases.delete(alias, cmd.config.command);
         });
         bot.commands.set(cmd.config.command, cmd); //set the thing
-        cmd.conf.aliases.forEach(alias => {
+        cmd.config.aliases.forEach(alias => {
             //set aliases
             bot.aliases.set(alias, cmd.config.command);
         });
     } catch (e) {
-        return msg.channel.createMessage(`Unable to reload: ${args[0]} because ` + e);
+        return msg.channel.send(`Unable to reload: ${args[0]} because ` + e);
     }
 
-    msg.channel.createMessage(`Successfully reloaded: ${args[0]}`);
+    msg.channel.send(`Successfully reloaded: ${args[0]}`);
 
 }
 exports.config = {
