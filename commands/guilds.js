b99d509e7c413188b8bd9d0245a7e130340b92d8
Glitch (yuuki-host-new) <none>
2018-08-21 01:57:23 +0000

    🐋🦃 Checkpoint ./commands/guilds.js:510018/112

--- commands/guilds.js
+++ commands/guilds.js
@@ -3,10 +3,11 @@ const { oneLineTrim } = require('common-tags')
 const columnify = require('columnify')
 const db = require('quick.db')
 exports.run = async (bot, message, args) => {
- var guilds = bot.guilds
+ var guilds = bot.guilds.size
+ var users = bot.users.size
  
  const embed = new MessageEmbed()
-     .setDescription(``)
+     .setDescription(`Estoy en ${guilds} y cuento con ${users} usuarios`)
  
    
     message.channel.send()
