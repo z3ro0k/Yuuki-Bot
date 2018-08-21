62fb144e9656e123724b6a33d4cfd15062296efd
Glitch (yuuki-host-new) <none>
2018-08-21 03:22:05 +0000

    🗻🚂 Checkpoint ./commands/github.js:510018/519 ./watch.json:510018/4 ./langs/es.json:510018/5 ./langs/en.json:510018/1

--- commands/github.js
+++ commands/github.js
@@ -4,13 +4,16 @@ const moment = require('moment');
 
 exports.run = async (bot, message, args) => {
   
+  var langg = await bot.tools.Lang(message.guild)    
+  const lang = require(`../langs/${langg}.json`) 
+  
 let name = args[0]
 	req('https://api.github.com/users/' + name, { headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0' }}, (e, r, b)=> {
 		let contenu = JSON.parse(b)
 		if(contenu.message === "Not Found") {
-			message.channel.send("This user doesn´t exist")
+			message.channel.send(lang.Github.nE)
 		} if (!name) {
-			message.channel.send("Please provide a user/org name")
+			message.channel.send(lang.Github.args)
 		} else {
       console.log(contenu)
 	const embed = new Discord.MessageEmbed()
@@ -18,13 +21,13 @@ let name = args[0]
 		embed.setColor(0x00AE86)
 		embed.setFooter(bot.user.username, bot.user.avatarURL);
 		embed.addField(contenu.login + " (" + contenu.type + ")", contenu.bio)     
-		embed.addField("Followers", contenu.followers, true)
-		embed.addField("Following", contenu.following, true)
-    embed.addField("Created on", moment(contenu.created_at).format("D MMMM Y"), true)
-    embed.addField("Location", contenu.location || 'Not Set', true)
-    embed.addField('Company', contenu.company || 'Nothing', true)  
-    embed.addField(contenu.public_repos + " public repos", "[Check them](https://github.com/" + name + "?tab=repositories)", true)	
-		embed.addField("Links", `${contenu.blog === null ? "Web Not set" : '[Website](' + contenu.blog  + ')'} | [Github](https://github.com/${contenu.login}') `)	
+		embed.addField(lang.Github.infoEmbed.f, contenu.followers, true)
+		embed.addField(lang.Github.infoEmbed.F, contenu.following, true)
+    embed.addField(lang.Github.infoEmbed.C , moment(contenu.created_at).format("D MMMM Y"), true)
+    embed.addField(lang.Github.infoEmbed.L , contenu.location || 'Not Set', true)
+    embed.addField(lang.Github.infoEmbed.Com , contenu.company || 'Nothing', true)  
+    embed.addField(contenu.public_repos + lang.Github.infoEmbed.rP , "[Check them](https://github.com/" + name + "?tab=repositories)", true)	
+		embed.addField("Links", `${contenu.blog === null ?lang.Github.infoEmbed.WebN : '[Website](' + contenu.blog  + ')'} | [Github](https://github.com/${contenu.login}') `)	
 		embed.setThumbnail(contenu.avatar_url)
 		embed.setTimestamp()
 		message.channel.send({embed});
