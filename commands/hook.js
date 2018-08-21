392ef569463bf0c50b87de2edcaef5c61c359577
Glitch (yuuki-host-new) <none>
2018-08-21 02:07:38 +0000

    ⏳🍳 Checkpoint ./commands/hook.js:510018/305

--- commands/hook.js
+++ commands/hook.js
@@ -1,14 +1,15 @@
-let prefix = 'Yu!'
+
 const tools = require('../functions.js');
 const db = require('quick.db')
-exports.run = (bot, message, args) => {
+exports.run = async (bot, message, args) => {
+  var prefix = await bot.tools.GuildPrefix(message.guild) 
         message.delete();
 
-         if (!args.join(' ')) { return tools.hook(message.channel,'Hook Usage', `${prefix}hook <title>, <message>, [HEXcolor], [avatarURL]\n\n**<> is required\n[] is optional**`,'FC8469','https://cdn4.iconfinder.com/data/icons/global-logistics-3/512/129-512.png') // Remeber that \n means new line. This is also using a custom HEX id, and an image.
+         if (!args.join(' ')) { return tools.hook(message.channel,'Hook Usage', `${prefix}hook <title>, <message>, [HEXcolor], [avatarURL]\n\n**<> is required\n[] is optional**`,'FC8469','https://cdn4.iconfinder.com/data/icons/global-logistics-3/512/129-512.png') 
         } 
-        let hookArgs = message.content.slice(prefix.length + 4).split(","); // This slices the first 6 letters (prefix & the word hook) then splits them by 'commas'
+        let hookArgs = message.content.slice(prefix.length + 4).split(","); 
 
-        tools.hook(message.channel, hookArgs[0], hookArgs[1], hookArgs[2], hookArgs[3]); // This is where it actually calls the hook.
+        tools.hook(message.channel, hookArgs[0], hookArgs[1], hookArgs[2], hookArgs[3]); 
 }
 
 exports.config = {
