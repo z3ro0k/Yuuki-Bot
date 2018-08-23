81b328fd5940894228d977b4644d18026eac0cb9
Glitch (yuuki-host-new) <none>
2018-08-23 07:08:35 +0000

    🌸🍄 Checkpoint ./commands/warn.js:510018/13744 ./commands/warnlist.js:510018/3573

--- commands/warnlist.js
+++ commands/warnlist.js
@@ -1,28 +1,62 @@
-const Discord = require("discord.js");
-const fs = require("fs");
-const ms = require("ms");
-let warns = JSON.parse(fs.readFileSync("./data/warnings.json", "utf8"));
+const db = require('quick.db');
+const Discord = require('discord.js')
+const send = require('discord.js')
+module.exports.run = async (client, message, args) => {
 
-exports.run = (bot, message, args, func) => {
+    if (!message.member.hasPermission("KICK_MEMBERS")) return send(message.channel, "Sorry, but you do not have valid permissions! If you believe this is an error, contact an owner.")
 
-  if(!message.member.hasPermission("KICK_MESSAGES")) return message.reply("<:adminNep:372599923381633024> **| No tienes Permisos para usar este comando.**");
-  let wUser = message.mentions.users.first() || bot.users.get(args[0])
-  if(!wUser) return message.reply("Couldn't find them yo");
-    if(!warns[wUser.id]) warns[wUser.id] = {
-    warns: 0
-  };
-  let warnlevel = warns[wUser.id].warns;
-     
-  const embed = new Discord.MessageEmbed()
-  .setTitle('Warns List')
-  .addField('Name', wUser.username)
-  .addField('Warnigs', warnlevel)
-  .setThumbnail(wUser.displayAvatarURL())
-  .setColor(0x36393e)
-  message.channel.send({ embed })
+    let user = message.mentions.users.first()
+    if (!user) return message.channel.send('**Please mention a user!**')
+
+    const numberwarn = new db.table('WARNNUMBERs')
+    numberwarn.fetch(`user_${user.id}_${message.guild.id}`).then(i => {
+
+        if (i === null) return message.channel.send(`**This user don't have any warnings**`)
+
+        if (!args[1]) return message.channel.send(`**Please specify which warning do you want to see! This user have ${i} warnings!**`)
+      
+        if (i === 5) return message.channel.send(`**Please specify which warning do you want to see! This user have ${i} warnings!**`)
+    })
+
+
+
+    const userwarns = new db.table('USERWARNINGs')
+    //const num2 = await numberwarn.fetch(`user_${user.id}`)
+    if (!args[1]) return;
+    const warns = await userwarns.fetch(`warn_${user.id}_${args[1]}_${message.guild.id}`)
+
+    const embed1 = new Discord.MessageEmbed()
+        .setAuthor(`${user.username}'s ${args[1]}st warning`)
+        .setDescription(`Reason: **${warns}**`)
+        .setColor(0x36393e)
+
+    const embed2 = new Discord.MessageEmbed()
+        .setAuthor(`${user.username}'s ${args[1]}nd warning`)
+        .setDescription(`Reason: **${warns}**`)
+        .setColor(0x36393e)
+
+    const embed3 = new Discord.MessageEmbed()
+        .setAuthor(`${user.username}'s ${args[1]}rd warning`)
+        .setDescription(`Reason: **${warns}**`)
+        .setColor(0x36393e)
+
+    const embed4 = new Discord.MessageEmbed()
+        .setAuthor(`${user.username}'s ${args[1]}th warning`)
+        .setDescription(`Reason: **${warns}**`)
+        .setColor(0x36393e)
+
+
+    if (args[1] === '1') return message.channel.send(embed1)
+    if (args[1] === '2') return message.channel.send(embed1)
+    if (args[1] === '3') return message.channel.send(embed1)
+
+    else {
+        message.channel.send(embed4)
+    }
 
 }
-module.exports.config = {
-  command: "warnlist",
-  aliases: ['warnlist', 'wlist']
+exports.config = {
+    command: "warns",
+    aliases: ['wlist', 'warnsl'],
+    category: "moderation"
 }
