e4e17c85d94f4de62674dd8f03ca8be136dad25d
Glitch (yuuki-host-new) <none>
2018-08-23 04:31:23 +0000

    🐏🎒 Checkpoint ./commands/poke.js:510018/93 ./watch.json:510018/6 ./commands/mevale.js:510018/7

--- commands/mevale.js
+++ commands/mevale.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 
 exports.run =  (bot, message, args, userAFK) => {
 
-  const image = new Discord.Attachment(`https://cdn.discordapp.com/attachments/336680413617455106/376134885695553556/gift.gif`, "me-vale.gif");
+  const image = new Discord.MessageAttachment(`https://cdn.discordapp.com/attachments/336680413617455106/376134885695553556/gift.gif`, "me-vale.gif");
 message.channel.send(image)
 
   }
--- commands/poke.js
+++ commands/poke.js
@@ -8,18 +8,18 @@ var member = msg.mentions.users.first() || msg.guild.members.get(args.join(' '))
       const pokeFetch = await fetch('https://nekos.life/api/v2/img/poke'),
         pokeImg = await pokeFetch.json();
 
-      return msg.embed({
+      return msg.channel.send({ embed: {
         description: member
           ? `${member.displayName}! You got poked by ${msg.member.displayName} 👉!` : `${msg.member.displayName} you must feel alone... Have a 🐈`,
         image: {url: member ? pokeImg.url : 'http://gifimage.net/wp-content/uploads/2017/06/anime-cat-gif-17.gif'},
         color: msg.guild ? msg.guild.me.displayColor : 10610610
-      }, `<@${member ? member.id : msg.author.id}>`);
+      }});
     } catch (err) {
   
       return msg.reply('something went wrong getting a poke image 💔');
     }
 }
 module.exports.config = {
-  command: "punch",
-  aliases: ['punch']
+  command: "poke",
+  aliases: ['poke']
 }
