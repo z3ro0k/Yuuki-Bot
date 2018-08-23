7414d301e4bc8c4e4be285ac7c218a70788ed6a9
Glitch (yuuki-host-new) <none>
2018-08-23 02:55:45 +0000

    ⛪️✋ Checkpoint ./commands/kiss.js:510018/2372

--- commands/kiss.js
+++ commands/kiss.js
@@ -1,22 +1,26 @@
 const Discord = require('discord.js')
 const db = require('quick.db')
-
+const fetch = require('node-fetch')
+      
 exports.run = async (bot, message, args) => {
   
-let elementos = ["https://i.imgur.com/F9zFLqf.gif", "https://i.imgur.com/F1LbtMM.gif", "https://i.imgur.com/dv9ES8R.gif", "https://i.imgur.com/T1jLvp9.gif", "https://i.imgur.com/a362Hxp.gif", "https://i.imgur.com/yYqKdR7.gif", "https://i.imgur.com/Js5YNQT.gif", "https://i.imgur.com/RWY3btA.gif", "https://i.imgur.com/dkdkWTF.gif", "https://i.imgur.com/xsmS0ER.gif", "https://i.imgur.com/kbzxaTj.gif", "https://i.imgur.com/ykOevpe.gif", "https://i.imgur.com/8IOxgsr.gif",
-                          "https://i.imgur.com/WIgZ3w3.gif", "https://i.imgur.com/PIImjpB.gif", "https://i.imgur.com/xAv1U7Y.gif", "https://i.imgur.com/jdbGHHt.gif", "https://i.imgur.com/2fcpVaP.gif", "https://i.imgur.com/d1xAcYy.gif", "https://i.imgur.com/2Qwdb8g.gif", "https://i.imgur.com/EcTg0i1.gif", "https://i.imgur.com/5xQ4cAh.gif", "https://i.imgur.com/mVULw8C.gif", "https://i.imgur.com/OdnNCK4.gif", "https://i.imgur.com/l7DjlGg.gif"]
-          let user = message.mentions.users.first()
-          
-          if(message.content.includes(message.author.id)) return message.reply('¿te besarás a ti mismo? o.O')
-          if(message.content.includes(bot.user.id)) return message.reply('N-no puedo hacerlo >u<')
-          
-          if (!user) return message.reply("lo siento >u<, pero debes mencionar a alguien.");
-            
-          const embed = new Discord.MessageEmbed()
-          .setDescription(`**${message.author.username}** le dio un beso a **${user.username}** o////o`)
-          .setImage(`${elementos[Math.floor(elementos.length * Math.random())]}`)
-          .setColor(0xCA3163)
-          message.channel.send({embed});
+  const kissFetch = await fetch('https://nekos.life/api/v2/img/kiss'),
+        kissImg = await kissFetch.json();
+
+  
+ return msg.embed({
+        description: member
+          ? `${member.displayName}! You were kissed by ${msg.member.displayName} 💋!` : `${msg.member.displayName} you must feel alone... Have a 🐈`,
+        image: {url: member ? kissImg.url : 'http://gifimage.net/wp-content/uploads/2017/06/anime-cat-gif-17.gif'},
+        color: msg.guild ? msg.guild.me.displayColor : 10610610
+      }, `<@${member ? member.id : msg.author.id}>`);
+  
+    } catch (err) {
+      stopTyping(msg);
+
+      return msg.reply('something went wrong getting a kiss image 💔');
+    }
+  }
 } 
 exports.config = {
   command: "kiss",
