b0aa567c642e6c344f9e2afb40eba0900ea0e0b2
Glitch (yuuki-host-new) <none>
2018-08-25 08:52:23 +0000

    🕰🍐 Checkpoint ./commands/radio.js:510018/14

--- commands/radio.js
+++ commands/radio.js
@@ -5,7 +5,7 @@ exports.run = async (bot, message, args) => {
  let voiceChannel = message.member.voiceChannel;
       if(!voiceChannel) return message.channel.send('¡Necesitas unirte a un canal de voz primero!.');
       voiceChannel.join().then(conexion =>{
-      conexion.playStream('http://stream.electroradio.fm:80/192k/;');
+      conexion.play('http://stream.electroradio.fm:80/192k/;');
       message.channel.send('<:umEmoji:440388171587649536> ** | Radio electro activado.**')
             return;
           }).catch(console.error);
