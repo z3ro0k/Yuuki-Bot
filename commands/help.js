4fe58aa2b10edb1f8ac37be7b5e50fe45ea23aec
Glitch (yuuki-host-new) <none>
2018-08-28 06:01:32 +0000

    📙🎂 Checkpoint ./commands/help.js:510018/1768

--- commands/help.js
+++ commands/help.js
@@ -1,22 +1,26 @@
 
 const Discord = require("discord.js");
 const { stripIndents } = require('common-tags');
+   
+    //let music = ['== Comandos de Musica ==\n\n◈ `Yu!play`    ::  Ponga en fila una canción/lista de reproducción por URL o busque una canción.\n◈ `Yu!queue`   :: Muestra la lista de canciones a reproducir\n◈ `Yu!search`   :: Busca hasta 10 resultados.\n◈ `Yu!skip`   :: Salta una canción o varias canciones.\n◈ `Yu!pause`   :: Pausa la cola.\n◈ `Yu!resume`   :: Reanuda la cola.\n◈ `Yu!volumen`   :: Ajusta el volumen del bot.\n◈ `Yu!np`   :: Muestra la canción que se está reproduciendo actualmente.\n◈ `Yu!clearqueue`   :: Borra la cola actual.\n◈ `Yu!leave`   :: Deja el canal de voz y limpia la cola.\n◈ `Yu!loop`   :: Cambia el estado del bucle']
+
+exports.run =  async (bot, message, args) => {
+  
+    var prefix = await bot.tools.GuildPrefix(message.guild) 
+    
     let pages = ['◈ `Yu!ping` ◈ `Yu!avatar` ◈ `Yu!ascii`◈ `Yu!calc◈` `Yu!emojis` ◈ `Yu!playing` ◈ `Yu!jumbo` ◈ `Yu!hug` ◈ `Yu!fortnite` ◈ `Yu!cat` ◈ `Yu!dog`'];
     let info = ['◈ `Yu!spotify` ◈ `Yu!weather` ◈ `Yu!help` ◈ `Yu!npm` ◈ `Yu!user` ◈ `Yu!ytsearch` ◈ `Yu!google` ◈ `Yu!mcuser` ◈ `Yu!roblox`']
     let admin = ['◈ `Yu!hook` ◈ `Yu!settings` ◈ `Yu!tmute` ◈ `Yu!warn` ◈ `Yu!warnlist` ◈ `Yu!clean`']
     let nsfw = ['◈ No hay comandos NSFW por el momento']
     let botC = ['◈ `Yu!report` ◈ `Yu!suggestion` ◈ `Yu!about`  ◈ `Yu!changelogs`']
     let server = ['◈ `Yu!welcome` ◈ `To!setautorole` ◈ `To!setchannel` ◈ `Yu!perms` ◈ `Yu!server` ◈ `Yu!starboard`']
-    let music = ['== Comandos de Musica ==\n\n◈ `Yu!play`    ::  Ponga en fila una canción/lista de reproducción por URL o busque una canción.\n◈ `Yu!queue`   :: Muestra la lista de canciones a reproducir\n◈ `Yu!search`   :: Busca hasta 10 resultados.\n◈ `Yu!skip`   :: Salta una canción o varias canciones.\n◈ `Yu!pause`   :: Pausa la cola.\n◈ `Yu!resume`   :: Reanuda la cola.\n◈ `Yu!volumen`   :: Ajusta el volumen del bot.\n◈ `Yu!np`   :: Muestra la canción que se está reproduciendo actualmente.\n◈ `Yu!clearqueue`   :: Borra la cola actual.\n◈ `Yu!leave`   :: Deja el canal de voz y limpia la cola.\n◈ `Yu!loop`   :: Cambia el estado del bucle']
-
-exports.run =  async (bot, message, args) => {
-
+    
     var command = bot.commands.get(args[0]) || bot.commands.get(bot.aliases.get(args[0]))
-  // await message.delete();
+  
 
 if(!command) {
      const embed = new Discord.MessageEmbed()
-      .setDescription(`<:helpNEP3:372992359287488512>**Mis comandos son **<:helpNEP3:372992359287488512>\nUse Yu!help comando para ver información detallada sobre el comando.`)
+      .setDescription(`<:help:483876577420247040>**Mis comandos son **<:help:483876577420247040>\nUse Yu!help comando para ver información detallada sobre el comando.`)
       .addField("== Comandos de Diversion ==", pages)
       .addField("== Comandos de información ==", info)
       .addField("== Comandos de administración ==", admin)
@@ -35,11 +39,12 @@ if(!command) {
 		 
 		} else {
   const embed2 = new Discord.MessageEmbed()
-  .addField("Comando:", `**${command.config.command}**`)
-  .addField("Alias:", `${command.config.aliases.join(', ') || 'None'}`)
-  .addField("Categoria:", `(\`${command.config.category || "None" }\`)` )
-  .addField("Descripción:", `**${command.config.description}**`) 
+  .setAuthor(`Comando/Command **${command.config.command}**`)
+  .setDescription(`**${command.config.description || "Nothing"}**`)
+  .addField("Alias", `${command.config.aliases.join(', ') || 'None'}`,true)
+  .addField("Categoria/Category", `(\`${command.config.category || "None" }\`)`, true)
   .setThumbnail(bot.user.displayAvatarURL())
+  .setColor(0xfcc7fb);
  return message.channel.send( embed2 );
     }
 	}
