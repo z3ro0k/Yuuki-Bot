459c2be9b2678fcc9372c35ba6c58a4fee8efa26
Glitch (yuuki-host-new) <none>
2019-03-20 22:04:10 +0000

    🌛🍗 Checkpoint ./yuuki.js:510018/604 ./watch.json:510018/2

--- yuuki.js
+++ yuuki.js
@@ -1,17 +1,14 @@
-const http = require('http');
-const express = require('express');
-const app = express();
+   const http = require('http');
+    const express = require('express');
+    const app = express();
+    app.get("/", (request, response) => {
+    response.sendStatus(200);
+    });
 
-app.use(express.static('public'));
-
-app.get("/", (request, response) => {
-  response.sendStatus(200);
-});
-
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/index.html');
-});
-
+   app.listen(process.env.PORT);
+  setInterval(() => {
+    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 10000);
 
 
 
