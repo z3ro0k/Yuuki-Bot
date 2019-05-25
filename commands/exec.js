bd432870765b64db76474d1d8f3c1d3c7804c663
Glitch (yuuki-host-new) <none>
2019-05-25 01:01:49 +0000

    🏋🐃 Checkpoint ./commands/exec.js:510018/1318 ./watch.json:510018/2

--- commands/exec.js
+++ commands/exec.js
@@ -1,19 +1,34 @@
 const exec = require('child_process').exec;
-const Discord = require('discord.js')
+const Discord = require('discord.js'),
+      hastebin = require('hastebin-gen')
+      
 exports.run = async(client, message, args, level) => {
     
-  if (message.author.id !== "322203879208910849" && message.author.id !== "331981819631239168") return message.channel.send("Only owners can use this command")
+  if (message.author.id !== "322203879208910849" && message.author.id !== "422208401020551173") return message.channel.send("Only owners can use this command")
     exec(`${args.join(' ')}`, (error, stdout) => {
-      
       const response = (error || stdout);
-      const embed = new Discord.MessageEmbed()
-      .setTitle('~ Exec Command ~')
-      .setDescription(`**Ran: ${args.join(" ")}**\n\`\`\`\js\n${response} \n\`\`\``, {code: "asciidoc", split: "\n"})
-      .setThumbnail(client.user.displayAvatarURL())
-      .setColor(0x36393e)
-      
-      message.channel.send({ embed }).catch(console.error);
-    
+      if (response.length > 1024 || response.length > 1024) {
+            
+          hastebin(`${response}`, "js").then(r => {
+              
+            const embed = new Discord.MessageEmbed()
+              .setTitle('~ Exec Command ~')`[\`${r}\`](${r})`
+              .setDescription(`**Ran: ${args.join(" ")}**\n [\`${r}\`](${r})`, {code: "asciidoc", split: "\n"})
+              .setThumbnail(client.user.displayAvatarURL())
+              .setColor(0x36393e)  
+            message.channel.send({ embed }).catch(console.error);
+            
+          })
+      } else {
+        
+        const embed = new Discord.MessageEmbed()
+              .setTitle('~ Exec Command ~')
+              .setDescription(`**Ran: ${args.join(" ")}**\n\`\`\`\js\n${response} \n\`\`\``, {code: "asciidoc", split: "\n"})
+              .setThumbnail(client.user.displayAvatarURL())
+              .setColor(0x36393e)  
+            message.channel.send({ embed }).catch(console.error);
+        
+      }
     });
 };
 exports.config = {
